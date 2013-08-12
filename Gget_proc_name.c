b56383373917be3d389dc76aa4713204208d5df2
Krystof Vasa <kvasa@me.com>
2013-08-12 13:21:57 +0100

    remove include

--- Gget_proc_name.c
+++ Gget_proc_name.c
@@ -24,7 +24,6 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
 #include "include/libunwind_i.h"
-#include "remote.h"
 
 static inline int
 intern_string (unw_addr_space_t as, unw_accessors_t *a,
