a2d65b0e5f21484bba62b250a0b5a504f232de11
Krystof Vasa <kvasa@me.com>
2013-08-12 11:14:56 +0100

    Ginit.c

--- Ginit.c
+++ Ginit.c
@@ -26,12 +26,6 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#include <stdlib.h>
-#include <string.h>
 #include <sys/mman.h>
 
 #include "unwind_i.h"
