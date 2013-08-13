f061c5a97363f6fbb261556ef9fa8d9bda89a722
Krystof Vasa <kvasa@me.com>
2013-08-13 09:14:44 +0100

    Trying to get proc name using the kernel linker

--- Gget_proc_name.c
+++ Gget_proc_name.c
@@ -24,75 +24,35 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
 #include "include/libunwind_i.h"
-#include "remote.h"
+#include <sys/linker.h>
 
-static inline int
-intern_string (unw_addr_space_t as, unw_accessors_t *a,
-	       unw_word_t addr, char *buf, size_t buf_len, void *arg)
-{
-  size_t i;
-  int ret;
+struct proc_name_search_result {
+  unw_word_t ip;
+  char *buf;
+  size_t buf_len;
+  long *offset;
+};
 
-  for (i = 0; i < buf_len; ++i)
-    {
-      if ((ret = fetch8 (as, a, &addr, (int8_t *) buf + i, arg)) < 0)
-	return ret;
-
-      if (buf[i] == '\0')
-	return 0;		/* copied full string; return success */
-    }
-  buf[buf_len - 1] = '\0';	/* ensure string is NUL terminated */
-  return -UNW_ENOMEM;
+static int linker_foreach(linker_file_t file, void *ctx) {
+  struct proc_name_search_result *result = (struct proc_name_search_result*)ctx;
+  if (linker_search_symbol_name(ctx->ip, ctx->buf, ctx->buf_len, buf->offset) == 0){
+    // Found it -> return 1 so that the foreach stops
+    return 1;
+  }
+  return 0;
 }
 
 static inline int
 get_proc_name (unw_addr_space_t as, unw_word_t ip,
 	       char *buf, size_t buf_len, unw_word_t *offp, void *arg)
 {
-  unw_accessors_t *a = unw_get_accessors (as);
-  unw_proc_info_t pi;
-  int ret;
-
-  buf[0] = '\0';	/* always return a valid string, even if it's empty */
-
-  ret = unwi_find_dynamic_proc_info (as, ip, &pi, 1, arg);
-  if (ret == 0)
-    {
-      unw_dyn_info_t *di = pi.unwind_info;
-
-      if (offp)
-	*offp = ip - pi.start_ip;
-
-      switch (di->format)
-	{
-	case UNW_INFO_FORMAT_DYNAMIC:
-	  ret = intern_string (as, a, di->u.pi.name_ptr, buf, buf_len, arg);
-	  break;
-
-	case UNW_INFO_FORMAT_TABLE:
-	case UNW_INFO_FORMAT_REMOTE_TABLE:
-	  /* XXX should we create a fake name, e.g.: "tablenameN",
-	     where N is the index of the function in the table??? */
-	  ret = -UNW_ENOINFO;
-	  break;
-
-	default:
-	  ret = -UNW_EINVAL;
-	  break;
-	}
-      unwi_put_dynamic_unwind_info (as, &pi, arg);
-      return ret;
-    }
-
-  if (ret != -UNW_ENOINFO)
-    return ret;
-
-  /* not a dynamic procedure, try to lookup static procedure name: */
-
-  if (a->get_proc_name)
-    return (*a->get_proc_name) (as, ip, buf, buf_len, offp, arg);
-
-  return -UNW_ENOINFO;
+  struct proc_name_search_result result;
+  result.ip = ip;
+  result.buf = buf;
+  result.buf_len = buf_len;
+  result.offset = offp;
+  
+  return linker_file_foreach(linker_foreach, &proc_name_search_result);
 }
 
 PROTECTED int
