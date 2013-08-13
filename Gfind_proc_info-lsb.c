40b0e431b9c7b5e0cc6d533e0ac623680399ba7f
Krystof Vasa <kvasa@me.com>
2013-08-13 14:03:52 +0100

    pragma weak?

--- Gfind_proc_info-lsb.c
+++ Gfind_proc_info-lsb.c
@@ -562,6 +562,7 @@ dwarf_find_proc_info (unw_addr_space_t as, unw_word_t ip,
   cb_data.di.format = -1;
   cb_data.di_debug.format = -1;
 
+#pragma weak dl_iterate_phdr
   SIGPROCMASK (SIG_SETMASK, &unwi_full_mask, &saved_mask);
   ret = dl_iterate_phdr (dwarf_callback, &cb_data);
   SIGPROCMASK (SIG_SETMASK, &saved_mask, NULL);
